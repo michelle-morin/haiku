7cc504206806cf38c3e431d2fd592cc9a09e39d9
Michelle Morin and Patrick Kille <michelle@>
2020-02-05 15:42:52 -0800

    add initial UI logic

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import { Haiku } from './haiku.js';
+import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    let inputPoem = document.getElementById("user-poem").value;
+    let userPoem = new Haiku();
+    userPoem.addPoem(inputPoem);
+    userPoem.doubleVowelRemover();
+    let linecount = userPoem.lineCount();
+    userPoem.wordSplit();
+    userPoem.silentVowelRemover();
+    userPoem.vowelCount();
+    console.log(userPoem);
+    if (linecount === false) {
+      $(".output").html("Not a haiku");
+    } else {
+      userPoem.checkHaiku();
+      if (userPoem.haiku === true) {
+        $(".output").html("Haiku!");
+      } else {
+        $(".output").html("Not a haiku!");
+      }
+    }
+  });
+});
